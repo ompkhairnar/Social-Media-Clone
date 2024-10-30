374284fb8d132c47f275866c9301df8037e2bc6b
bidit-acharyya <105981837+bidit-acharyya@users.noreply.github.com>
2024-10-30 18:34:49 -0400

    Added method headers for database

--- FoundationDatabase.java
+++ FoundationDatabase.java
@@ -1,2 +1,27 @@
 public class FoundationDatabase {
+  private ArrayList<User> users;
+  private String userFileName;
+
+  public FoundationDatabase(ArrayList<User> users, String filename) {
+    this.users = users;
+    userFileName = filename;
+  }
+
+  //Bidit
+  public boolean readUsers(String file) {
+  }
+  // Bidit
+  public boolean createUser(String username, String password) {
+  }
+  //Richard
+  public String viewUser(String username) {
+    // use search method to make sure that user exists
+  }
+  //Richard
+  public boolean search(String username) {
+  }
+  // Richard
+  public boolean deleteUser(String username, String password) {
+    // use search method to verify the user exists first
+  }
 }
