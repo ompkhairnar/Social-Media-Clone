b0c8d3cf1540c3ab47de7c5cb357bdcb0c3d34ec
richard <87430416+yaebk@users.noreply.github.com>
2024-10-30 19:07:12 -0400

    updated search and deleteUser functions
    added functionality

--- FoundationDatabase.java
+++ FoundationDatabase.java
@@ -17,11 +17,23 @@ public class FoundationDatabase {
   public String viewUser(String username) {
     // use search method to make sure that user exists
   }
+  
   //Richard
   public boolean search(String username) {
+    for (User user : users) {
+        if (user.getUsername().equalsIgnoreCase(username)) {  // case-insensitive + using user method
+            return true;
+        }
+    }
+    return false;
   }
   // Richard
   public boolean deleteUser(String username, String password) {
     // use search method to verify the user exists first
+    if (search(username)) {
+      if(username.getPassword().equals(password)) {
+        users.remove(username);
+      }
+    }
   }
 }
