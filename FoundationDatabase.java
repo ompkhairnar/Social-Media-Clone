430daa8c9de1187598ac67f987342747ff4ebc2a
Richard Seungho Yom <ryom@data.cs.purdue.edu>
2024-10-30 19:20:11 -0400

    added functionality and comments to deleteUser, viewUser, and search functions

--- FoundationDatabase.java
+++ FoundationDatabase.java
@@ -16,12 +16,26 @@ public class FoundationDatabase {
   //Richard
   public String viewUser(String username) {
     // use search method to make sure that user exists
+  	if (search(username)) {
+  		return username;
+    	}
   }
   //Richard
-  public boolean search(String username) {
+  public boolean search(String username) {	
+	for (User user : users) {
+        	if (user.getUsername().equalsIgnoreCase(username)) {  // case-insensitive + using user method
+            		return true;
+        	}
+    	}
+  	return false;
   }
   // Richard
   public boolean deleteUser(String username, String password) {
     // use search method to verify the user exists first
+  	if (search(username)) {
+  		if (username.getPassword().equals(password)) { // uses getPassword from user class
+        		users.remove(username);
+        	}
+    	}
   }
 }
