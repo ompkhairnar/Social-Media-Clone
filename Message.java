f4f2cad24bdf705dc1a04d2b30bd2fa6b607b7da
ompkhairnar <opkhairnar@gmail.com>
2024-11-01 16:01:05 -0400

    Created message class and interface

--- Message.java
+++ Message.java
@@ -1,20 +1,59 @@
-public class Message {
+import java.io.*;
+
+public class Message implements MessageInterface {
     private User messager;
+    private File messages;
+
     public Message(User messager) throws UserException {
-//        if(usernotfound) {
-//            throw new usernotfound;
-//        }
+        if (!isValidUser(messager)) {
+            throw new UserException("Invalid user");
+        }
         this.messager = messager;
     }
-    public void messageUser(User user, String message){
-        /*check if user is valid
-        check if file betwen users already exists
-        if not then create new file
-        then add the message to the file in a new line
-        */
+
+    public void messageUser(User user, String message) {
+        try {
+            if (isValidUser(user)) {
+                String fileName = messager.getUsername() + user.getUsername() + "messages";
+                File file = new File(fileName);
+                if (file.exists()) {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                    writer.write(message);
+                    writer.newLine();
+                } else {
+                    file.createNewFile();
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                    writer.write(message);
+                    writer.newLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
     public User getMessager() {
         return messager;
+    }
+
+    public String getMessages(User user) {
+        String messages = "";
+        try {
+            File file = new File(messager.getUsername() + user.getUsername() + "messages");
+            if (!file.exists()) {
+                return "No messages currenly exist between users";
+            } else {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    messages += line + "\n";
+                }
+                return messages;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return messages;
     }
 
 }
--- MessageInterface.java
+++ MessageInterface.java
@@ -1,3 +1,9 @@
 public interface MessageInterface {
+    public void messageUser(User user, String message);
+
+    public User getMessager();
+
+    public String getMessages(User user);
+
 
 }
