b0bd446df86047a6a6751979053c6f22e00f6718
bidit-acharyya <105981837+bidit-acharyya@users.noreply.github.com>
2024-11-17 18:38:09 -0500

    added message server functionality to social media server

--- SocialMediaServer.java
+++ SocialMediaServer.java
@@ -131,6 +131,57 @@ public class SocialMediaServer implements Runnable, SocialMediaServerInterface {
                             done = true; // exits loop
                             out.println("Goodbye!"); // send goodbye to client
                             break;
+                        
+                        case "5":
+                            try {
+                                ServerSocket serverSocket = new ServerSocket(4242);
+                                Socket socket = serverSocket.accept();
+                                BufferedReader clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                                PrintWriter clientWriter = new PrintWriter(socket.getOutputStream());
+                    
+                                System.out.println("HEY");
+                                String textingUser = "";
+                                textingUser = clientReader.readLine();
+                                String sentMsg = "";
+                                System.out.println("HI");
+                                do {
+                                    sentMsg = clientReader.readLine();
+                                    System.out.println(textingUser);
+                                    BufferedReader textReader = new BufferedReader(new FileReader("messages.csv"));
+                                    ArrayList<String> messages = new ArrayList<String>();
+                                    ArrayList<String> receivedIndices = new ArrayList<String>();
+                                    String descString = ""; 
+                    
+                                    int i = 0;
+                                    String line = textReader.readLine();
+                                    while (line != null) {
+                                        String[] contents = line.split(";");
+                                        String sender = contents[0];
+                                        String receiver = contents[1];
+                                        String message = contents[3];
+                                        if (receiver.equals(textingUser)) {
+                                            messages.add(message);
+                                        } else if (sender.equals(textingUser)) {
+                                            messages.add(message);
+                                            receivedIndices.add(i + "");
+                                        }
+                                        i++;
+                                        line = textReader.readLine();
+                                    }
+                                    textReader.close();
+                                    messages.add(sentMsg);
+                                    String messageString = String.join("\n", messages);
+                                    String indexString = String.join("\n", receivedIndices);
+                                    clientWriter.write(messageString);
+                                    clientWriter.println();
+                                    clientWriter.flush();
+                                    clientWriter.write(indexString);
+                                    clientWriter.println();
+                                    clientWriter.flush();
+                                } while (sentMsg != null);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
 
                         default:
                             out.println("Invalid choice. Please try again.");
