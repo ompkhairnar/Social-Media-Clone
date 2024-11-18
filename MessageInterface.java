41e7a9bbbd8da21f3f5ac864dc963417c2bd43e8
richard <87430416+yaebk@users.noreply.github.com>
2024-11-17 21:54:24 -0500

    added method

--- MessageInterface.java
+++ MessageInterface.java
@@ -2,5 +2,6 @@ public interface MessageInterface {
     public void messageUser(User user, String message);
     public User getMessager();
     public String getMessages(User user);
+    public String getContent();
     public void updateFile(User user, String newMessage) throws UserException;
 }
